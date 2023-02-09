0e7ab86b737321b21374bbb56dab6f5bf166902b
John Maybee <john.d.maybee@gmail.com>
2023-02-09 14:14:32 -0500

    Started looking through the current files, and started a note list within the genre.router.js to start laying out ideas and what I think needs to be updated in this route to get the genre information.

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -8,3 +8,10 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+// Need to work on this file. 
+// movie.router has a query set to get all of the movies/ppsters and provides a route to get '/' displays all of the movies
+// [] need to add to the router.get to send a query to get all of the genres from 
+  // This is going to be similar to the movie.router, but the req.body.___ is going to be different
+  // Next thought is to see where we are supposed to display the genres within the instructions.
+  // rn it seems like the router is set to get the information on page load. Do we need to display them on the DOM here too?
