a0511c7926ee88ab1170f8f589997aaeda1b53e3
John Maybee <john.d.maybee@gmail.com>
2023-02-11 14:30:49 -0500

    realized after looking back through the package.json that express hadn't been installed as a dependency, so I went ahead and installed that as some of the files are utilizing express. Next I will look through the the full list of components and the index.js to see what all else might be missing before I add in the other components.

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.use(express.static('build'));
 app.use('/api/movie', movieRouter);
 app.use('/api/genre', genreRouter);
 // do we need to add an app.use here for the movies_genres table and add a route as well?
+// also, is the '/api' necessary to utilize? We haven't listed that w/in routes before.
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
