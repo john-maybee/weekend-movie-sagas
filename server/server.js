c5b0c5a5caf29777ad39836ee8be9fa958c4c369
John Maybee <john.d.maybee@gmail.com>
2023-02-11 13:55:49 -0500

    added some corrections to ther server.js file, so that lines are ended properly. Also added notes in case we need to make a route to the movies_genres table.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const movieRouter = require('./routes/movie.router.js')
-const genreRouter = require('./routes/genre.router.js')
+const movieRouter = require('./routes/movie.router.js');
+const genreRouter = require('./routes/genre.router.js');
 const port = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -11,7 +11,8 @@ app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
 app.use('/api/movie', movieRouter);
-app.use('/api/genre', genreRouter)
+app.use('/api/genre', genreRouter);
+// do we need to add an app.use here for the movies_genres table and add a route as well?
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
