f1a8e74f5d8462bae35a98429165d37c9828f8ed
John Maybee <john.d.maybee@gmail.com>
2023-02-11 13:52:37 -0500

    added a router.get to the genre.router that will access the varying genres on page load.

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,14 +4,27 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
-});
+  const query = `SELECT * FROM genres ORDER BY "name" ASC`;
+  pool.query(query)
+    .then( result => {
+      console.log(result.rows);
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all genres', err);
+      res.sendStatus(500)
+    })
+
+}); // end of router.get that sends a query that selects all genres from the DB
+    // this router.get gets the genres on site load due to '/'
+
+// if you get to the stretch goals, then add a router.post right here
 
 module.exports = router;
 
 // Need to work on this file. 
 // movie.router has a query set to get all of the movies/ppsters and provides a route to get '/' displays all of the movies
-// [] need to add to the router.get to send a query to get all of the genres from 
+// [X] need to add to the router.get to send a query to get all of the genres from 
   // This is going to be similar to the movie.router, but the req.body.___ is going to be different
   // Next thought is to see where we are supposed to display the genres within the instructions.
   // rn it seems like the router is set to get the information on page load. Do we need to display them on the DOM here too?
